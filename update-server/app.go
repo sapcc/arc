1542e7034776917ab905a8ab5020213e3e4b52be
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-05-29 15:05:05 +0200

    update server with cli

--- update-server/app.go
+++ update-server/app.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 	"encoding/json"
 	"fmt"
-	log "github.com/Sirupsen/logrus"
 	"gitHub.***REMOVED***/monsoon/arc/update-server/updates"
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-const BuildsRootPath = "/Users/userID/go/src/gitHub.***REMOVED***/monsoon/arc/update-server/builds"
+var BuildsRootPath string
 const BuildRelativeUrl = "/builds/"
 
 type Build struct {
@@ -18,6 +20,63 @@ type Build struct {
 }
 
 func main() {
+	app := cli.NewApp()
+
+	app.Name = "update-server"
+	app.Authors = []cli.Author{
+		{
+			Name:  "Fabian Ruff",
+			Email: "fabian.ruff@sap.com",
+		},
+		{
+			Name:  "Arturo Reuschenbach Puncernau",
+			Email: "a.reuschenbach.puncernau@sap.com",
+		},
+	}
+	app.Usage = "web server to to check and deliver from updates"
+	app.Version = "0.1.0-dev"
+  app.Action = runServer
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "log-level,l",
+			Usage:  "log level",
+			Value:  "info",
+		},		
+		cli.StringFlag{
+			Name:   "builds-path",
+			Usage:  "Path to builds in the file system",
+		},
+		cli.StringFlag{
+			Name:   "server-port",
+			Usage:  "Update server port",
+			Value: "3000",
+		},
+	}
+	
+	app.Before = func(c *cli.Context) error {
+		lvl, err := log.ParseLevel(c.GlobalString("log-level"))
+		if err != nil {
+			log.Fatalf("Invalid log level: %s\n", c.GlobalString("log-level"))
+			return err
+		}
+		log.SetLevel(lvl)
+		return nil
+	}
+	
+	app.Run(os.Args)
+}
+
+// private
+
+func runServer(c *cli.Context) {
+	// check mandatory params
+	if len(c.GlobalString("builds-path")) == 0 {
+		log.Fatalf("Build path is missing. Got %q", c.GlobalString("builds-path"))
+		return
+	} 
+	
+	BuildsRootPath = c.GlobalString("builds-path")
+	
 	// api
 	http.HandleFunc("/updates", availableUpdates)
 
@@ -31,8 +90,8 @@ func main() {
 	// serve template
 	http.HandleFunc("/", serveTemplate)
 
-	log.Infof("Listening...")
-	http.ListenAndServe(":3000", nil)
+	log.Infof("Listening on port %q...", c.GlobalString("server-port"))
+	http.ListenAndServe( fmt.Sprint(":", c.GlobalString("server-port")), nil)
 }
 
 func availableUpdates(w http.ResponseWriter, r *http.Request) {
@@ -97,8 +156,6 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-
-// private
 
 func getAllBuilds() *[]string {
 	var fileNames []string
