be3026cee6d1d0b140a64c016874c504099ed1ec
Fabian Ruff <fabian.ruff@sap.com>
2015-06-01 14:13:24 +0200

    run porwershell scripts with -Command instead of -File
    Fixes #2

--- agents/execute/script_windows.go
+++ agents/execute/script_windows.go
@@ -7,6 +7,6 @@ import "gitHub.***REMOVED***/monsoon/arc/arc"
 var scriptSuffix = ".ps1"
 
 func scriptCommand(file string) *arc.Subprocess {
-	process := arc.NewSubprocess("powershell.exe", "-ExecutionPolicy", "RemoteSigned", "-File", file)
+	process := arc.NewSubprocess("powershell.exe", "-NoProfile", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", "-Command", "$ErrorActionPreference = 'Stop'; & "+file)
 	return process
 }
