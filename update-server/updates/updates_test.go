44aa2a0bc7ad4dc5121d5512c5bf1ec93d78f1b1
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-06-04 14:06:06 +0200

    [update-server]: tests with temp builds

--- update-server/updates/updates_test.go
+++ update-server/updates/updates_test.go
@@ -2,104 +2,119 @@ package updates
 
 import (
 	"bytes"
+	"github.com/inconshreveable/go-update/check"
+	"io/ioutil"
 	"net/http"
-	"testing"
-
-	"path"
-	"runtime"
+	"os"
 	"strings"
+	"testing"
 )
 
 func TestUpdatesNewSuccess(t *testing.T) {
-	// get path to the builds
-	_, filename, _, _ := runtime.Caller(0)
-	buildsPath := strings.Replace(path.Dir(filename), "updates", "builds", 1)
+	file, err := ioutil.TempFile(os.TempDir(), "arc_darwin_amd64_3.1.0-dev_")
+	defer os.Remove(file.Name())
 
 	// get a success update
 	var jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"arch":"amd64","os":"darwin"}}`) //
 	req, _ := http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update := New(req, buildsPath)
+	update, err := New(req, os.TempDir())
+	if err != nil {
+		t.Error("Expected not get an error. Got ", err)
+	}
 	if update == nil {
-		t.Error("Expected update NOT to be nil. Got %q", update)
+		t.Error("Expected update NOT to be nil. Got ", update)
 	}
 
 	if update.Initiative != "automatically" {
-		t.Error("Expected Initiative to be 'automatically'. Got %q", update.Initiative)
+		t.Error("Expected Initiative to be 'automatically'. Got ", update.Initiative)
 	}
-	if update.Url != "http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev" {
-		t.Error("Expected Initiative to be 'http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev'. Got %q", update.Url)
+
+	if !strings.HasPrefix(update.Url, "http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev") {
+		t.Error("Expected url to be 'http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev'. Got ", update.Url)
 	}
+
 	if update.Version != "3.1.0-dev" {
-		t.Error("Expected Initiative to be '3.1.0-dev'. Got %q", update.Version)
+		t.Error("Expected version to be '3.1.0-dev'. Got ", update.Version)
 	}
 }
 
 func TestUpdatesNewReturnNil(t *testing.T) {
+	var update *check.Result
+	var req *http.Request
+	var err error
+
 	// post request host is missing or wrong
-	req, _ := http.NewRequest("POST", "", bytes.NewBufferString(""))
-	update := New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
-	}
-	req, _ = http.NewRequest("POST", "miau", bytes.NewBufferString(""))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+	hosts := []string{"", "miau"}
+	for _, h := range hosts {
+		req, _ = http.NewRequest("POST", h, bytes.NewBufferString(""))
+		update, err = New(req, "/some/build/path/")
+		if err == nil {
+			t.Error("Expected err to be nil when testing wrong hosts. Got ", err)
+		}
+		if update != nil {
+			t.Error("Expected update to be nil when testing wrong hosts. Got ", update)
+		}
 	}
 
 	// post request body is empty
 	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBufferString(""))
-	update = New(req, "/some/build/path/")
+	update, err = New(req, "/some/build/path/")
+	if err == nil {
+		t.Error("Expected err to be nil when testing empty body. Got ", err)
+	}
 	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+		t.Error("Expected update to be nil when testing empty body. Got ", update)
 	}
 
 	// check that the body is json
 	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBufferString("not json"))
-	update = New(req, "/some/build/path/")
+	update, err = New(req, "/some/build/path/")
+	if err == nil {
+		t.Error("Expected err to be nil when testing json body. Got ", err)
+	}
 	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+		t.Error("Expected update to be nil when testing json body. Got ", update)
 	}
 
 	// check that the body has not the required params
-	jsonStr := []byte(`{"param1":"param1"}`)
-	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+	requiredParams := []string{
+		`{"param1":"param1"}`,
+		`{"app_id":"arc"}`,
+		`{"app_id":"arc","app_version":"0.1.0-dev"}`,
+		`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"os":"darwin"}}`,
 	}
-	jsonStr = []byte(`{"app_id":"arc"}`)
-	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
-	}
-	jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev"}`)
-	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
-	}
-	jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"os":"darwin"}}`)
-	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+	var jsonStr []byte
+	for _, p := range requiredParams {
+		jsonStr = []byte(p)
+		req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
+		update, err = New(req, "/some/build/path/")
+		if err == nil {
+			t.Error("Expected err to be nil when testing required params. Got ", err)
+		}
+		if update != nil {
+			t.Error("Expected update to be nil when testing required params. Got ", update)
+		}
 	}
 
 	// check wrong version format
 	jsonStr = []byte(`{"app_id":"arc","app_version":"miau","tags":{"arch":"amd64","os":"darwin"}}`) //
 	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
+	update, err = New(req, "/some/build/path/")
+	if err == nil {
+		t.Error("Expected err to be nil when testing wrong version format. Got ", err)
+	}
 	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+		t.Error("Expected update to be nil when wrong version format. Got ", update)
 	}
 
 	// check wrong build path
 	jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"arch":"amd64","os":"darwin"}}`) //
 	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
+	update, err = New(req, "/some/build/path/")
+	if err == nil {
+		t.Error("Expected err to be nil when testing wrong build path. Got ", err)
+	}
 	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
+		t.Error("Expected update to be nil when wrong build path. Got ", update)
 	}
 }
