749f1cd25aa64d73dddc62968ea213f53b85233e
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-06-04 11:23:09 +0200

    updates.go tests finished

--- update-server/updates/updates_test.go
+++ update-server/updates/updates_test.go
@@ -4,18 +4,34 @@ import (
 	"bytes"
 	"net/http"
 	"testing"
+
+	"path"
+	"runtime"
+	"strings"
 )
 
 func TestUpdatesNewSuccess(t *testing.T) {
 	// get path to the builds
+	_, filename, _, _ := runtime.Caller(0)
+	buildsPath := strings.Replace(path.Dir(filename), "updates", "builds", 1)
 
 	// get a success update
-	/*jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"arch":"amd64","os":"darwin"}}`) //
-	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
-	update = New(req, "/some/build/path/")
-	if update != nil {
-		t.Error("Expected update to be nil. Got %q", update)
-	}*/
+	var jsonStr = []byte(`{"app_id":"arc","app_version":"0.1.0-dev","tags":{"arch":"amd64","os":"darwin"}}`) //
+	req, _ := http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
+	update := New(req, buildsPath)
+	if update == nil {
+		t.Error("Expected update NOT to be nil. Got %q", update)
+	}
+
+	if update.Initiative != "automatically" {
+		t.Error("Expected Initiative to be 'automatically'. Got %q", update.Initiative)
+	}
+	if update.Url != "http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev" {
+		t.Error("Expected Initiative to be 'http://0.0.0.0:3000/builds/arc_darwin_amd64_3.1.0-dev'. Got %q", update.Url)
+	}
+	if update.Version != "3.1.0-dev" {
+		t.Error("Expected Initiative to be '3.1.0-dev'. Got %q", update.Version)
+	}
 }
 
 func TestUpdatesNewReturnNil(t *testing.T) {
@@ -46,7 +62,7 @@ func TestUpdatesNewReturnNil(t *testing.T) {
 	}
 
 	// check that the body has not the required params
-	var jsonStr = []byte(`{"param1":"param1"}`)
+	jsonStr := []byte(`{"param1":"param1"}`)
 	req, _ = http.NewRequest("POST", "http://0.0.0.0:3000/updates", bytes.NewBuffer(jsonStr))
 	update = New(req, "/some/build/path/")
 	if update != nil {
