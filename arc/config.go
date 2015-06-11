85949da64efb6e129e2fc374d8efd8ad2d31d9fa
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-06-11 15:24:35 +0200

    config struct to string

--- arc/config.go
+++ arc/config.go
@@ -1,5 +1,9 @@
 package arc
 
+import(
+	"fmt"
+)
+
 type Config struct {
 	Endpoints  []string
 	ClientCa   string
@@ -9,4 +13,8 @@ type Config struct {
 	Identity   string
 	Project    string
 	LogLevel   string
+}
+
+func (c Config) String() string {
+	return fmt.Sprintf("Endpoints: %s, ClientCa: %s, ClientCert: %s, ClientKey: %s, Transport: %s, Identity: %s, Project: %s, LogLevel: %s", c.Endpoints, c.ClientCa, c.ClientCert, c.ClientKey, c.Transport, c.Identity, c.Project, c.LogLevel)
 }
