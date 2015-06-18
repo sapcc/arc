9d1627e10580d99a9020ca17393d327692bd063b
Fabian Ruff <fabian.ruff@sap.com>
2015-06-18 12:41:49 +0200

    suse platform detection was fixed upstream

--- fact/host/host_unix.go
+++ fact/host/host_unix.go
@@ -7,7 +7,6 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/shirou/gopsutil/common"
 	"github.com/shirou/gopsutil/host"
 )
 
@@ -37,35 +36,5 @@ func (h Source) Facts() (map[string]interface{}, error) {
 		}
 	}
 
-	if common.PathExists("/etc/SuSE-release") {
-		contents, err := common.ReadLines("/etc/SuSE-release")
-		if err == nil {
-			facts["platform_family"] = "suse"
-			facts["platform"] = getSusePlatform(contents)
-			facts["platform_version"] = getSuseVersion(contents)
-
-		}
-	}
-
 	return facts, nil
-}
-
-func getSuseVersion(contents []string) string {
-	version := ""
-	for _, line := range contents {
-		if matches := regexp.MustCompile(`VERSION = ([\d.]+)`).FindStringSubmatch(line); matches != nil {
-			version = matches[1]
-		} else if matches := regexp.MustCompile(`PATCHLEVEL = ([\d]+)`).FindStringSubmatch(line); matches != nil {
-			version = version + "." + matches[1]
-		}
-	}
-	return version
-}
-
-func getSusePlatform(contents []string) string {
-	c := strings.ToLower(strings.Join(contents, ""))
-	if strings.Contains(c, "opensuse") {
-		return "opensuse"
-	}
-	return "suse"
 }
