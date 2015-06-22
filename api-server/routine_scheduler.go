7a3fb4b401ef1cd106cbe53a9d456ecf4f144900
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-06-22 18:05:05 +0200

    fmt

--- api-server/routine_scheduler.go
+++ api-server/routine_scheduler.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"time"
-	"fmt"
 	"database/sql"
-	
+	"fmt"
+	"time"
+
 	log "github.com/Sirupsen/logrus"
-	
+
 	ownDb "gitHub.***REMOVED***/monsoon/arc/api-server/db"
 )
 
@@ -14,7 +14,7 @@ func routineScheduler(db *sql.DB) error {
 	if db == nil {
 		return fmt.Errorf("Db connection is nil")
 	}
-	
+
 	cleanJobsChan := time.NewTicker(time.Second * 60)
 
 	for {
@@ -28,20 +28,20 @@ func routineScheduler(db *sql.DB) error {
 
 func cleanJobs(db *sql.DB) {
 	log.Info("Clean job routine started")
-	
+
 	// clean jobs which no heartbeat was send back after created_at + 60 sec
 	res, err := db.Exec(ownDb.CleanJobsNonHeartbeatQuery, 60)
 	if err != nil {
 		log.Error(err.Error())
 	}
-	
+
 	affect, err := res.RowsAffected()
 	if err != nil {
 		log.Error(err.Error())
-	}	
-	
+	}
+
 	log.Infof("%v jobs without heartbeat answer where updated", affect)
-	
+
 	// clean jobs which the timeout + 60 sec has exceeded and still in queued or executing status
 	res, err = db.Exec(ownDb.CleanJobsTimeoutQuery, 60)
 	if err != nil {
