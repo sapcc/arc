96ff44f4ddca852124aadedf85acf2d0a0d6fe69
Fabian Ruff <fabian.ruff@sap.com>
2015-08-11 16:37:31 +0200

    Add fact for init system detection on linux

--- fact/host/host_linux.go
+++ fact/host/host_linux.go
@@ -1,6 +1,12 @@
 package host
 
-import "github.com/shirou/gopsutil/host"
+import (
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	"github.com/shirou/gopsutil/host"
+)
 
 func (h Source) Facts() (map[string]interface{}, error) {
 
@@ -21,6 +27,33 @@ func (h Source) Facts() (map[string]interface{}, error) {
 	if fqdn, domain := fqdn_and_domain(); fqdn != "" {
 		facts["fqdn"] = fqdn
 		facts["domain"] = domain
+	}
+
+	//init system detection
+	if contents, err := ioutil.ReadFile("/proc/1/comm"); err == nil && strings.TrimSpace(string(contents)) == "systemd" {
+		//upstart and sysvinit report "init" so we can only use this for systemd
+		facts["init_package"] = "systemd"
+	} else {
+		var cmd *exec.Cmd
+		switch facts["platform_family"] {
+		case "debian":
+			cmd = exec.Command("dpkg", "-S", "/sbin/init")
+		case "rhel", "fedora", "suse":
+			cmd = exec.Command("rpm", "--qf", "%{name}", "-qf", "/sbin/init")
+		}
+		if cmd != nil {
+			if out, err := cmd.Output(); err == nil {
+				pkg_name := string(out)
+				switch {
+				case strings.Contains(pkg_name, "systemd"):
+					facts["init_package"] = "systemd"
+				case strings.Contains(pkg_name, "upstart"):
+					facts["init_package"] = "upstart"
+				case strings.Contains(pkg_name, "sysv"):
+					facts["init_package"] = "sysv"
+				}
+			}
+		}
 	}
 
 	return facts, nil
