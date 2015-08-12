f608ed1c30753051d9b6e1e54de2bc07425bb454
Fabian Ruff <fabian.ruff@sap.com>
2015-08-12 10:53:04 +0200

    Add windows service installation
    This uses the NSSM binary with is bundled into the app

---
+++ service/service.go
@@ -0,0 +1,5 @@
+package service
+
+var serviceName = "arc"
+var serviceDisplayName = "Arc Agent" //mostly for windows
+var serviceDescription = "Monsoon remote control agent"
--- service/service_windows.go
+++ service/service_windows.go
@@ -1,5 +1,84 @@
 package service
 
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/kardianos/osext"
+)
+
 func Install(dir string) error {
-	panic("Not implemented on this platform")
+	executable, err := osext.Executable()
+	if err != nil {
+		return errors.New("Can't locate running executable")
+	}
+
+	if err := os.MkdirAll(path.Join(dir, "log"), 0755); err != nil {
+		return err
+	}
+
+	return installNSSM(executable, dir)
+
+}
+
+func installNSSM(executable, installDir string) error {
+	log.Info("Installing the NSSM supervisor")
+	nssm := filepath.Join(installDir, "nssm.exe")
+	err := ioutil.WriteFile(nssm, FSMustByte(false, "/nssm.exe"), 0755)
+	if err != nil {
+		return err
+	}
+
+	//Remove any previously created service
+	exec.Command(nssm, "stop", serviceName).Run()
+	exec.Command(nssm, "remove", serviceName, "confirm").Run()
+
+	installArgs := []string{"install", serviceName, executable, "-c", path.Join(installDir, "arc.cfg"), "server"}
+	log.Debugf("Running %s %s", nssm, strings.Join(installArgs, " "))
+	if out, err := exec.Command(nssm, installArgs...).CombinedOutput(); err != nil {
+		return fmt.Errorf("Failed to install service: %s err: %v", filterNullBytes(out), err)
+	}
+
+	settings := [][]string{
+		[]string{"Description", serviceDescription},
+		[]string{"DisplayName", serviceDisplayName},
+		[]string{"AppStdout", path.Join(installDir, "log", "current")},
+		[]string{"AppStderr", path.Join(installDir, "log", "current")},
+		[]string{"AppRotateFiles", "1"},
+		[]string{"AppRotateBytes", "100000"},
+		[]string{"AppRotateOnline", "1"},
+		[]string{"AppStopMethodSkip", "6"},
+		[]string{"AppStopMethodConsole", "2000"},
+	}
+	for _, s := range settings {
+		if out, err := exec.Command(nssm, "set", serviceName, s[0], s[1]).CombinedOutput(); err != nil {
+			return fmt.Errorf("Failed to set service option %s: %s", s[0], string(out))
+		}
+	}
+
+	log.Info("Starting service")
+	if out, err := exec.Command(nssm, "start", serviceName).CombinedOutput(); err != nil {
+		return fmt.Errorf("Failed to start service: %s", filterNullBytes(out))
+	}
+
+	return nil
+}
+
+// No idea why, but the output of the nssm binary execution contains a null byte after every character
+// we filter this out to keep the errors readble
+func filterNullBytes(in []byte) []byte {
+	filtered := make([]byte, 0, len(in)/2)
+	for _, b := range in {
+		if b != 0 {
+			filtered = append(filtered, b)
+		}
+	}
+	return filtered
 }
