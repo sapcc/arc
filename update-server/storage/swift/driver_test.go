c51cbefd4cc6f3e6340a7c1c4361f1dd0630f9ee
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-09-08 14:36:34 +0200

    [update-sever]: added test file for swift storage

---
+++ update-server/storage/swift/driver_test.go
@@ -0,0 +1,27 @@
+// +build !integration
+
+package swift
+
+import (
+  "testing"
+	"flag"
+	
+	"github.com/codegangsta/cli"
+)
+
+// 
+// New()
+// 
+
+func TestNewMissingParams(t *testing.T) {
+	localSet := flag.NewFlagSet("test", 0)
+	ctx := cli.NewContext(nil, localSet, nil)
+	
+	storage, err := New(ctx)	
+	if err == nil {
+		t.Error("Expected to have an error")
+	}
+	if storage != nil {
+		t.Error("Expected to have nil swift storage")
+	}
+}
