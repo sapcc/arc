028b83d3cea3185c13c5ee1c29ffa3d5243336fb
Fabian Ruff <fabian.ruff@sap.com>
2015-09-23 14:12:24 +0200

    use PKI_SERVICE_HOST/PORT when dynamically generating the transport cert for the api

--- api-server/env.sh
+++ api-server/env.sh
@@ -5,10 +5,11 @@ if [ -f "$ENV_FILE" ]; then
   echo Sourcing environment file "$ENV_FILE" ...
   . "$ENV_FILE"
 fi
-
-if [ ! -z $PKI_ENDPOINT ]; then
-	DIR=`dirname $ARC_TLS_CA_CERT`
-	gencert --pki-endpoint=$PKI_ENDPOINT --output-dir=$DIR --common-name=$COMMON_NAME
+# if we want to use certs but the given cert does not exist try to create one from the pki service
+if [ ! -z "$ARC_TLS_CLIENT_CERT" ] && [ ! -f "$ARC_TLS_CLIENT_CERT" ] && [ ! -z "$PKI_SERVICE_HOST" ] && [ ! -z "$COMMON_NAME" ]; then
+	DIR=`dirname $ARC_TLS_CLIENT_CERT`
+	echo gencert --pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT --output-dir=$DIR --common-name=$COMMON_NAME transport
+	gencert --pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT --output-dir=$DIR --common-name=$COMMON_NAME transport
 fi
 
 exec "$@"
