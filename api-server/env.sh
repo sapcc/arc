babaa9850d79b994a35e6734b6758bf4b24c46e0
Fabian Ruff <fabian.ruff@sap.com>
2015-09-23 14:31:39 +0200

    fix gencert for real :|

--- api-server/env.sh
+++ api-server/env.sh
@@ -11,7 +11,7 @@ fi
 if [ ! -z "$ARC_TLS_CLIENT_CERT" ] && [ ! -f "$ARC_TLS_CLIENT_CERT" ] && [ ! -z "$PKI_SERVICE_HOST" ] && [ ! -z "$COMMON_NAME" ]; then
 	DIR=`dirname $ARC_TLS_CLIENT_CERT`
 	echo gencert -pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT -output-dir=$DIR -cn=$COMMON_NAME transport
-	gencert --pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT --output-dir=$DIR --common-name=$COMMON_NAME transport
+	gencert -pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT -output-dir=$DIR -cn=$COMMON_NAME transport
 fi
 
 exec "$@"
