1b4153e3b820a943da344b6553a7da5f8d7df8f7
Fabian Ruff <fabian.ruff@sap.com>
2015-09-23 14:19:45 +0200

    idomatic go flags

--- api-server/env.sh
+++ api-server/env.sh
@@ -10,7 +10,7 @@ fi
 # TODO: Think about authentication at some point
 if [ ! -z "$ARC_TLS_CLIENT_CERT" ] && [ ! -f "$ARC_TLS_CLIENT_CERT" ] && [ ! -z "$PKI_SERVICE_HOST" ] && [ ! -z "$COMMON_NAME" ]; then
 	DIR=`dirname $ARC_TLS_CLIENT_CERT`
-	echo gencert --pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT --output-dir=$DIR --cn=$COMMON_NAME transport
+	echo gencert -pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT -output-dir=$DIR -cn=$COMMON_NAME transport
 	gencert --pki-endpoint=http://$PKI_SERVICE_HOST:$PKI_SERVICE_PORT --output-dir=$DIR --common-name=$COMMON_NAME transport
 fi
 
