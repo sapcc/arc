3e56aab937091d6792dff833c3d39bd0244119a3
Fabian Ruff <fabian.ruff@sap.com>
2015-09-30 11:55:56 +0200

    The updated integration test now retries a few timeout before failing
    THis gives the agents some time to actually detect and fetch the update

--- integration-tests/updated_online_service_test.go
+++ integration-tests/updated_online_service_test.go
@@ -7,13 +7,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"gitHub.***REMOVED***/monsoon/arc/api-server/models"
 )
 
-var arcDeployVersionFlag = flag.String("latest-version", "2015.01.01", "integration-test")
+var arcLatestVersion = flag.String("latest-version", "2015.01.01", "integration-test")
+var timeout = flag.Int("timeout", 20, "timeout waiting for agents to update")
 
 type checkFacts struct {
 	Version string `json:"arc_version"`
@@ -22,9 +25,12 @@ type checkFacts struct {
 
 func TestAgentsAreUpdatedAndOnline(t *testing.T) {
 	// override flags if enviroment variable exists
-	if os.Getenv("LATEST_VERSION") != "" {
-		deployVersion := os.Getenv("LATEST_VERSION")
-		arcDeployVersionFlag = &deployVersion
+	if e := os.Getenv("LATEST_VERSION"); e != "" {
+		arcLatestVersion = &e
+	}
+	if os.Getenv("TIMEOUT") != "" {
+		i, _ := strconv.Atoi(os.Getenv("TIMEOUT"))
+		timeout = &i
 	}
 
 	// get the logged agents
@@ -43,28 +49,42 @@ func TestAgentsAreUpdatedAndOnline(t *testing.T) {
 	}
 
 	// check the version from each agent
-	for i := 0; i < len(agents); i++ {
-		statusCode, body = client.GetApiV1(fmt.Sprint("/agents/", agents[i].AgentID, "/facts"), ApiServer)
-		if statusCode != "200 OK" {
-			t.Error(fmt.Sprint("Expected to get 200 response code getting facts for agent ", agents[i]))
-			continue
-		}
 
-		var facts checkFacts
-		err = json.Unmarshal(*body, &facts)
-		if err != nil {
-			t.Error(fmt.Sprint("Expected not to get an error unmarshaling for agent ", agents[i]))
-			continue
-		}
+	for i := 0; i < *timeout; i++ {
 
-		// check version
-		if !strings.Contains(facts.Version, *arcDeployVersionFlag) {
-			t.Error(fmt.Sprint("Expected to match versions for agent ", agents[i].AgentID, ". Got environment version ", *arcDeployVersionFlag, " and fact version ", facts.Version))
-		}
+		for i, agent := range agents {
+			statusCode, body = client.GetApiV1(fmt.Sprint("/agents/", agent.AgentID, "/facts"), ApiServer)
+			if statusCode != "200 OK" {
+				t.Error("Expected to get 200 response code getting facts for agent ", agent)
+				agents = append(agents[:i], agents[:i+1]...)
+				continue
+			}
 
-		// check online
-		if facts.Online == false {
-			t.Error(fmt.Sprint("Expected agent ", agents[i].AgentID, " to be online."))
+			var facts checkFacts
+			err = json.Unmarshal(*body, &facts)
+			if err != nil {
+				t.Error("Error unmarshaling response for ", agent)
+				agents = append(agents[:i], agents[:i+1]...)
+				continue
+			}
+
+			// check version
+			if !strings.Contains(facts.Version, *arcLatestVersion) {
+				fmt.Printf("Agent %s is running version %s, expected %s\n", agent.AgentID, *arcLatestVersion, facts.Version)
+				continue
+			}
+
+			// check online
+			if facts.Online == false {
+				t.Error(fmt.Sprint("Expected agent ", agent.AgentID, " to be online."))
+			}
+			fmt.Printf("Agent %s is online and updated\n", agent.AgentID)
+			agents = append(agents[:i], agents[:i+1]...)
 		}
+		fmt.Println("Sleeping for 1 second...")
+		time.Sleep(1 * time.Second)
+	}
+	for _, agent := range agents {
+		t.Errorf("Agent %s failed to update before %ds timeout was reached", agent.AgentID, *timeout)
 	}
 }
