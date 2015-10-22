5e75b9afd61048416c75d0631c8d0dd116025edf
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2015-10-22 15:42:45 +0200

    fmt

--- integration-tests/smoke_test.go
+++ integration-tests/smoke_test.go
@@ -3,12 +3,12 @@
 package integrationTests
 
 import (
-	"testing"
-	"os"
-	"fmt"
-	"flag"
 	"bytes"
+	"flag"
+	"fmt"
+	"os"
 	"strings"
+	"testing"
 )
 
 var arcLatestVersion = flag.String("latest-version", "2015.01.01", "integration-test")
@@ -18,7 +18,7 @@ func TestApiServerIsUp(t *testing.T) {
 	if e := os.Getenv("LATEST_VERSION"); e != "" {
 		arcLatestVersion = &e
 	}
-	
+
 	client, err := NewTestClient()
 	if err != nil {
 		t.Fatal(err)
@@ -29,12 +29,12 @@ func TestApiServerIsUp(t *testing.T) {
 	if statusCode != expected {
 		t.Errorf("Expected to get %#v code for the ApiServer. Got %#v", expected, statusCode)
 	}
-	
-	bodystring := bytes.NewBuffer(*body).String()	
+
+	bodystring := bytes.NewBuffer(*body).String()
 	if !strings.Contains(bodystring, *arcLatestVersion) {
 		fmt.Printf("ApiServer is running version %#v, expected %#v\n", bodystring, *arcLatestVersion)
 	}
-	
+
 }
 
 func TestUpdateServerIsUp(t *testing.T) {
@@ -48,8 +48,8 @@ func TestUpdateServerIsUp(t *testing.T) {
 	if statusCode != expected {
 		t.Errorf("Expected to get %#v code for the UpdateServer. Got %#v", expected, statusCode)
 	}
-	
-	bodystring := bytes.NewBuffer(*body).String()	
+
+	bodystring := bytes.NewBuffer(*body).String()
 	if !strings.Contains(bodystring, *arcLatestVersion) {
 		fmt.Printf("UpdateServer is running version %#v, expected %#v\n", bodystring, *arcLatestVersion)
 	}
