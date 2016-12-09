bcada94c84ef78b905daf46bbcbdc0c6471f35ad
Fabian Ruff <fabian.ruff@sap.com>
2016-12-09 12:43:00 +0100

    fix integration-tests build

---
+++ integration-tests/run_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# vim: set ft=sh
+
+set -e
+require_env () {
+  val=$(eval echo \${$1})
+  if [ -z $val ]; then
+    echo "$1 environment variable missing"
+    exit 1
+  fi
+}
+
+
+require_env "API_SERVER"
+require_env "API_VERSION"
+require_env "ARC_VERSION"
+require_env "KEYSTONE_ENDPOINT"
+require_env "DOMAIN"
+require_env "PROJECT"
+require_env "USERNAME"
+require_env "PASSWORD"
+
+TOKEN=$(get-token)
+
+echo "Running smoke tests"
+smoke
+
+echo "Checking that agents are running the current version"
+updated-test
+
+for id in $(curl -s -H X-Auth-Token:$TOKEN $API_SERVER/api/v1/agents | jq -r '.[]|.agent_id')
+do
+  echo "Running job test for $id:"
+  AGENT_IDENTITY=$id job-test
+  echo "Running fact test for $id:"
+  AGENT_IDENTITY=$id fact-test
+done
+
