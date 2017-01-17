3b251b3ca241491ab4e44d8a99267d0f7b7df414
Fabian Ruff <fabian.ruff@sap.com>
2017-01-17 11:50:18 +0100

    remove cert generation from seperate pki service

--- api-server/entrypoint.sh
+++ api-server/entrypoint.sh
@@ -2,16 +2,6 @@
 set -e
 
 if [ "$1" = "api-server" ]; then
-  # if we want to use certs but the given cert does not exist try to create one from the pki service
-  # TODO: Think about authentication at some point
-  PKI_HOST=${ARC_PKI_SERVICE_HOST:-$PKI_SERVICE_HOST}
-  PKI_PORT=${ARC_PKI_SERVICE_PORT:-$PKI_SERVICE_PORT}
-  if [ ! -z "$ARC_TLS_CLIENT_CERT" ] && [ ! -f "$ARC_TLS_CLIENT_CERT" ] && [ ! -z "$PKI_HOST" ] && [ ! -z "$COMMON_NAME" ]; then
-    DIR=`dirname $ARC_TLS_CLIENT_CERT`
-    echo gencert -pki-endpoint=http://$PKI_HOST:$PKI_PORT -output-dir=$DIR -cn=$COMMON_NAME transport
-    gencert -pki-endpoint=http://$PKI_HOST:$PKI_PORT -output-dir=$DIR -cn=$COMMON_NAME transport
-  fi
-
   # run migrations
   goose -env=$ARC_ENV status
   goose -env=$ARC_ENV up
