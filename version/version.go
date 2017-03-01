92066956fc88a75e259d9ced687dc7516c587a3b
Fabian Ruff <fabian.ruff@sap.com>
2017-03-01 12:03:14 +0100

    Add ci tasks for building janitor

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ import (
 //needs to be a var (no const)
 //so that we van overwrite during linking with -X main.GITCOMMIT ...
 var (
-	Version   = "20150910.01"
+	Version   = ""
 	GITCOMMIT = "HEAD"
 )
 
