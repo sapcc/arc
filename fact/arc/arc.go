ab06754d8e11155fa5c5a51faabb2aa0b834f267
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2017-04-26 11:31:45 +0200

    [facts]: read certificate from config

--- fact/arc/arc.go
+++ fact/arc/arc.go
@@ -1,15 +1,6 @@
 package arc
 
 import (
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path"
-	"path/filepath"
-
 	arc_config "gitHub.***REMOVED***/monsoon/arc/config"
 	"gitHub.***REMOVED***/monsoon/arc/version"
 )
@@ -29,41 +20,16 @@ func (h Source) Name() string {
 func (h Source) Facts() (map[string]interface{}, error) {
 	facts := make(map[string]interface{})
 
-	// build path to the cert.pem
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return facts, fmt.Errorf("Failed to build path to the cert.pem file: %v", err)
-	}
-	certPath := path.Join(dir, "cert.pem")
-
-	// read cert.pem
-	pemCert, err := ioutil.ReadFile(certPath)
-	if err != nil {
-		return facts, fmt.Errorf("Failed to read %s: %v", certPath, err)
-	}
-
-	// decode
-	certData, _ := pem.Decode(pemCert)
-	if certData == nil {
-		return facts, errors.New("Failed to parse PEM encoded certificate.")
-	}
-
-	// parse
-	x509Cert, err := x509.ParseCertificate(certData.Bytes)
-	if err != nil {
-		return facts, err
-	}
-	certSubject := x509Cert.Subject
-
 	facts["arc_version"] = version.String()
-	if len(certSubject.OrganizationalUnit) > 0 {
-		facts["project"] = certSubject.OrganizationalUnit[0]
+
+	if len(h.config.Project) > 0 {
+		facts["project"] = h.config.Project
 	}
-	if certSubject.CommonName != "" {
-		facts["identity"] = certSubject.CommonName
+	if len(h.config.Identity) > 0 {
+		facts["identity"] = h.config.Identity
 	}
-	if len(certSubject.Organization) > 0 {
-		facts["organization"] = certSubject.Organization[0]
+	if len(h.config.Organization) > 0 {
+		facts["organization"] = h.config.Organization
 	}
 	return facts, nil
 }
