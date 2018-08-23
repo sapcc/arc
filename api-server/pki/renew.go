7aaa681b462e279bd0c207738914d4d796ef6d1a
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-08-23 14:20:54 +0200

    Remove comment

--- api-server/pki/renew.go
+++ api-server/pki/renew.go
@@ -32,14 +32,6 @@ func RenewCert(cfg *arc_config.Config, renewURI string, renewThreshold int64, ht
 		return false, hoursLeft, nil
 	}
 
-	start := time.Now()
-	certs, err := x509.SystemCertPool()
-	end := time.Now()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("found %d certs in %v\n", len(certs.Subjects()), end.Sub(start))
-
 	// #nosec: TLS InsecureSkipVerify set true
 	// client creation
 	client := &http.Client{
