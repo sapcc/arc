4ed99bb24c48527012e9562d2c3dfbb45c3b20ab
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-09-07 10:19:32 +0200

    debug prepareCmd for win

--- arc/subprocess_windows.go
+++ arc/subprocess_windows.go
@@ -1,11 +1,16 @@
 package arc
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func (s *Subprocess) Kill() {
 	s.cmd.Process.Kill()
 }
 
 func (s *Subprocess) prepareCmd() *exec.Cmd {
-	return exec.Command(s.Command[0], s.Command[1:]...) // #nosec
+	cmd := exec.Command(s.Command[0], s.Command[1:]...) // #nosec
+	fmt.Printf("%+v\n", cmd)
+	return cmd
 }
