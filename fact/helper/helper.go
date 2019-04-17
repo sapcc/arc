cc815e9f85791d3aedb95e7df94c45be7caff343
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-04-17 10:54:26 +0200

    fixed gosec warning

--- fact/helper/helper.go
+++ fact/helper/helper.go
@@ -5,7 +5,7 @@ import "unsafe"
 // BytePtrToString converts byte pointer to a Go string.
 // borrowed from github.com/shirou/gopsutil/internal/common
 func BytePtrToString(p *uint8) string {
-	a := (*[10000]uint8)(unsafe.Pointer(p))
+	a := (*[10000]uint8)(unsafe.Pointer(p)) // #nosec
 	i := 0
 	for a[i] != 0 {
 		i++
