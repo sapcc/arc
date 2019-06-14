9e7e239efd0f710c697caaee43e336183e75cd1b
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-06-14 11:52:21 +0200

    arc ci migration: curl certs if no ssh key given

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-set -o errexit 
+set -o errexit
 if [ "$SSH_KEY" != "" ]; then
   mkdir -p ~/.ssh
   chmod 700 ~/.ssh
   echo "$SSH_KEY" > ~/.ssh/id_rsa #the quotes prevent word splitting
   chmod 600 ~/.ssh/id_rsa
+else
+  curl -fL http://aia.pki.co.sap.com/aia/SAP%20Global%20Root%20CA.crt | tr -d '\r' > /usr/local/share/ca-certificates/SAP_Global_Root_CA.crt && update-ca-certificates
 fi
 if [ "$1" = "" ]; then
   echo "usage: $0 [WEBSITE DIRECTORY]"
