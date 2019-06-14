baf0582beaa09e3dace8b206968c5c24f6c42759
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-06-14 13:06:14 +0200

    arc ci fix test

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-set -o errexit 
+set -o errexit
 if [ "$SSH_KEY" != "" ]; then
   mkdir -p ~/.ssh
   chmod 700 ~/.ssh
   echo "$SSH_KEY" > ~/.ssh/id_rsa #the quotes prevent word splitting
   chmod 600 ~/.ssh/id_rsa
+  # hack since concourse5 can not deal with git@ protocol when getting the sources
+  git remote set-url origin git@gitHub.***REMOVED***:monsoon/arc.git
 fi
 if [ "$1" = "" ]; then
   echo "usage: $0 [WEBSITE DIRECTORY]"
