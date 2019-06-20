162dd5708892760d4de64f29af2b0373ebdc59c4
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-06-20 10:25:08 +0200

    removed unused data

--- chef/site-cookbooks/golang/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['golang']['version']= '1.4.2'
-default['golang']['installer_filename'] = "go#{node['golang']['version']}.windows-amd64.msi"
-default['golang']['installer_url']= "https://storage.googleapis.com/golang/#{node['golang']['installer_filename']}"
-default['golang']['dev_pkg']='gitHub.***REMOVED***/monsoon/arc'
-
-
