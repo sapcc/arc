0638dfc8739efe405a727caf1a05924615ab81f1
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2021-10-25 17:01:08 +0200

    added metrics for mqtt messages

--- api-server/arc.go
+++ api-server/arc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/sapcc/arc/api-server/models"
 	"github.com/sapcc/arc/arc"
@@ -10,6 +11,29 @@ import (
 	"github.com/sapcc/arc/transport/fake"
 	"github.com/sapcc/arc/transport/helpers"
 )
+
+var (
+	metricMessageCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "arc_api_mqtt_messages_total",
+			Help: "Total number of received mqtt messages.",
+		},
+		[]string{"type"},
+	)
+	metricMessageDurationsHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "arc_api_mqtt_message_duration_seconds",
+			Help:    "Latency of processing MQTT message.",
+			Buckets: prometheus.LinearBuckets(0.01, 0.05, 10),
+		},
+		[]string{"message"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(metricMessageCounter)
+	prometheus.MustRegister(metricMessageDurationsHistogram)
+}
 
 /*
  * Returns a transport connection
@@ -47,14 +71,23 @@ func arcSubscribeReplies(tp transport.Transport) error {
 		select {
 		case registry := <-regChan:
 			log.Debugf("Got registration from %q with id %q and data %q", registry.Sender, registry.RegistrationID, registry.Payload)
+			timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+				metricMessageDurationsHistogram.WithLabelValues("registration").Observe(v)
+			}))
 
+			// add registration
 			err := models.ProcessRegistration(db, registry, tp.IdentityInformation().Identity, concurrencySafe)
+			// save processing time
+			timer.ObserveDuration()
+			// count registration
+			metricMessageCounter.WithLabelValues("registration").Inc()
+			// check errors
 			if _, ok := err.(models.RegistrationExistsError); ok {
 				log.Debug(err.Error(), " Registration id ", registry.RegistrationID)
 			} else {
 				if err != nil {
 					log.Errorf("error updating registration %q. Got %q", registry.RegistrationID, err.Error())
-				}
+				}				
 			}
 
 			// send done signal (for testing)
@@ -64,17 +97,25 @@ func arcSubscribeReplies(tp transport.Transport) error {
 			}
 		case reply := <-msgChan:
 			log.Infof("Got reply with id %q and status %q", reply.RequestID, reply.State)
+			timer := prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+				metricMessageDurationsHistogram.WithLabelValues("reply").Observe(v)
+			}))
 
 			// add log
 			err := models.ProcessLogReply(db, reply, tp.IdentityInformation().Identity, concurrencySafe)
+			// save processing time
+			timer.ObserveDuration()
+			// count reply
+			metricMessageCounter.WithLabelValues("reply").Inc()			
+			// check errors
 			if _, ok := err.(models.ReplyExistsError); ok {
 				log.Debug(err.Error(), " Reply id ", reply.RequestID)
-			} else {
+			} else {				
 				if err != nil {
 					log.Error(err)
 					continue
 				}
-			}
+			}			
 
 			// send done signal (for testing)
 			ftp, ok := tp.(*fake.FakeClient)
